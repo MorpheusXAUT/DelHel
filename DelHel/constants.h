968b0f75ff71370b2047f9ff4a273457b22caf27
Nick 'MorpheusXAUT' Müller <nick@morpheusxaut.net>
2021-06-17 21:49:56 +0200

    Version bump to v0.0.6

--- DelHel/constants.h
+++ DelHel/constants.h
@@ -3,7 +3,7 @@
 #include <regex>
 
 #define PLUGIN_NAME    "DelHel"
-#define PLUGIN_VERSION "0.0.5"
+#define PLUGIN_VERSION "0.0.6"
 #define PLUGIN_AUTHOR  "Nick Mueller"
 #define PLUGIN_LICENSE "(c) 2020, MIT License"
 #define PLUGIN_LATEST_VERSION_URL "https://raw.githubusercontent.com/MorpheusXAUT/DelHel/master/version.txt"
